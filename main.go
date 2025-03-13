81b6a96d48bef923026098fef9b2112b3c0d3009
Mason Putney <mputney38n@nextgenid.com>
2025-03-12 18:23:48 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"log"
+)
+
+type Game struct{}
+
+func (game *Game) Update() error {
+	return nil
+}
+
+func (game *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (game *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	game := &Game{}
+
+	err := ebiten.RunGame(game)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
